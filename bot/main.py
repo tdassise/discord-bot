3e3d33a892ca0c8c772d73407ace289aa9d59c47
tdassise <tibo.d37@gmail.com>
2022-10-19 11:25:19 +0200

    first commit

--- bot/main.py
+++ bot/main.py
@@ -6,15 +6,25 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='titi ', intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}({bot.user.id})')
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+@bot.command(name="coucou")
+async def coucou(ctx):
+    reponse = f"Ça va, {ctx.message.author.name} ?"
+    await ctx.reply(reponse)
+    print(f"Réponse à message {ctx.message.id} : {reponse}")
+
 
 if __name__ == '__main__':
     bot.run(os.getenv('DISCORD_TOKEN'))
